f8420362fefc0c6b75a0aeef2d687b3d73a67a7c
sharfuz95 <sshifat@u.rochester.edu>
2018-11-28 16:11:17 -0500

    initial commit

---
+++ actions.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Past Events</title>
+		<link type="text/css" rel="stylesheet" href="css/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=PT+Serif|Playfair+Display" rel="stylesheet">
+
+	</head>
+	<body>
+
+	 <?php include "inc/nav.inc"; ?>
+
+
+		<main>
+
+				
+		<div class="event">
+			
+		<div class="event-grid-item-block">
+			
+		<h2>The April Social</h2>
+					<img src="images/social.jpg" alt="A picture of Brandon">
+			</div>
+				
+				<div class="event-grid-item-block">	
+					<h3>Details</h3>
+					<p>The April Social is a networking event for Entrepreneurs to meet students interested in participating in the Mark Ain Business Model Competition or the Forbes Entrepreneurial Competition. 
+					These two comeptitions are the biggest business competitions that are organized by the University of Rochester 
+					and are usually in the Spring. One of the biggest barriers to enter the competition is finding the right team.
+					That is why we organized this event in order to create networking opportunities so students can 
+					step out of their circles, meet new people and participate with some cool ideas. 
+</p>
+
+			
+					<p> Date: April 11th, 2017 </p>
+					<p>Location: Welles Brown Room. </p>
+				
+				</div>
+
+				<div class="event-grid-item-block">
+
+					<h3>Experience</h3>
+					<p> "I had a really great time. Glad to see we have a proper entrepreneurship club now" - Sandesh Paudel</p>
+					<p> "It was great hearing about other students ideas. I found a team member and we are hoping to particiapte at the competitions" - Riley Barkles</p>
+				</div>
+
+</div>
+				
+<div class="event">
+		<div class="event-grid-item-block">
+		<h2>Startup Spotlight: Pollinate</h2>
+			<img src="images/pollinate-event.png" alt="The official flyer for Pollinat event">
+			
+		<div>
+			
+		</div>
+		<section class="basic">	
+			
+		</section>
+		</div>
+		
+		<div class="event-grid-item-block">	
+			<h3>Details</h3>
+			<p> 
+					Meliora LaunchPad is pleased to present its first event of the semester: a chance to be a part of Pollinate's official Launch Party! Pollinate was a founded by UR alumni and we feel very fortunate bring you this event two years in the making. Stop by for FREE Chipotle burritos, FREE Pollinate T-shirts, opportunities to connect with representatives from Pollinate's nonprofit partners, and your first chance to join the Pollinate Panel.
+
+					What is the Pollinate Panel?
+					Students who join the Pollinate Panel have the opportunity to earn money through their participation in market research studies. Panel members participate in 10-15 minute interviews, earn $4-$8 per interview, and then have the opportunity to do one of three things:
+						
+			</p>
+			<p> Date: Sept 8th, 2017 </p>
+			<p>Location: Rettner Atrium </p>
+		
+		</div>
+
+		<div class="event-grid-item-block">
+
+			<h3>Experience</h3>
+			<p> "Thanks to Meliora LaunchPad we got the dream launch event to our startup" - Max Wolf(Founder, Pollinate)</p>
+			<p> "I had no idea my peers were working on such cool startups. It definitely inspired me" - Zoli Bustamante</p>
+	</div>
+
+</div>
+
+
+<div class="event">
+		<div class="event-grid-item-block">
+		<h2>Entrepreneurship Expo</h2>
+			<img src="images/expo.jpg" alt="The official flyer for Entrepreneurship Expo even">
+			
+		<div>
+			
+		</div>
+		<section class="basic">	
+			
+		</section>
+		</div>
+		
+		<div class="event-grid-item-block">	
+			<h3>Details</h3>
+			<p> 
+					For the first time ever, will bring together student entrepreneurs on our campus to showcase their entrepreneurial projects to our entire community. This event will uniquely bring together the entrepreneurial community, allow us to appreciate, support and give feedback to the student entrepreneurs who have been working on their diverse projects as well as join their ventures. There will also be amazing Professors from our business department, staff and many more esteemed guests and members of our community that students will get the chance to network with.
+
+					If you are interested in entrepreneurship, innovation, ideas or just want to meet some amazing students, faculty and want to be a part of the first Entrepreneurship Expo at the University of Rochester, this event is for you. Join us and celebrate entrepreneurship with us this evening. We hope to see you there!
+
+			</p>
+			<p> Date: April 25th, 2017 </p>
+			<p>Location: Feldman Ball Room</p>
+		
+		</div>
+
+		<div class="event-grid-item-block">
+
+			<h3>Experience</h3>
+			<p>"This is the biggest entrepreneurship related event I have ever seen on this school" - Noah Tyson</p>
+			<p>" Never thought there were so many students working on startups in our school. Got to meet some really cool people and Professor" - Connor Dyler</p>
+
+		</div>
+
+</div>
+
+
+		</main>
+
+        <?php include "inc/footer.inc"; ?>
+
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Global Warming</title>
+        <link type="text/css" rel="stylesheet" href="css/styles.css">
+</head>
+
+	<body>
+    <? php include "inc/nav.inc"; ?>
+   
+     
+    <h1> What is Global Warming</h1> 
+     
+        <div class="home-grid-container">
+        <div class="home-grid-item">
+            <h1> About</h1>
+            <p>
+            Glaciers are melting, sea levels are rising, cloud forests are dying, and wildlife is scrambling to keep pace. It's becoming clear that humans have caused most of the past century's warming by releasing heat-trapping gases as we power our modern lives. Called greenhouse gases, their levels are higher now than in the last 650,000 years.
+        We call the result global warming, but it is causing a set of changes to the Earth's climate, or long-term weather patterns, that varies from place to place. As the Earth spins each day, the new heat swirls with it, picking up moisture over the oceans, rising here, settling there. It's changing the rhythms of climate that all living things have come to rely upon. [Watch Polar Bears 101].
+        What will we do to slow this warming? How will we cope with the changes we've already set into motion? While we struggle to figure it all out, the face of the Earth as we know it—coasts, forests, farms and snow-capped mountains—hangs in the balance.
+                </p>
+        </div>
+        
+
+        <div class = "home-grid-item">
+            <h1> History</h1>
+            <p>
+            The history of climate change science began in the early 19th century when ice ages and other natural changes in paleoclimate were first suspected and the natural greenhouse effect first identified.[64] In the late 19th century, scientists first argued that human emissions of greenhouse gases could change the climate. In the 1960s, the warming effect of carbon dioxide gas became increasingly convincing.[279] By the 1990s, as a result of improving fidelity of computer models and observational work confirming the Milankovitch theory of the ice ages, a consensus position formed: greenhouse gases were deeply involved in most climate changes and human caused emissions were bringing discernible global warming. Since the 1990s, scientific research on climate change has included multiple disciplines and has expanded.[280] Research during this period has been summarized in the Assessment Reports by the Intergovernmental Panel on Climate Change.
+            </p>
+        </div>
+
+
+        <div class = "home-grid-item" id="join">
+                <h1> Interested in Entrepreneurship?</h1>
+                <h2> Lets get in touch!!!!</h2>
+                <form>
+                        First name:<br>
+                        <input type="text" name="firstname">
+                        <br>
+                        Last name:<br>
+                        <input type="text" name="lastname">
+                        <br>
+                        Email:<br>
+                        <input type="text" name="email">
+                        <br>
+                        <button> Join Newsletter </button>
+
+                 </form>
+
+
+            </div>
+    
+    </div>
+
+
+
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+	</body>
+
+</html>
