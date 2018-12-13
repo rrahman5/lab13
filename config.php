554734ebc90329c225c920fec1ea9e82173c7cdd
Reeshad Rahman <rrahman5@u.rochester.edu>
2018-12-12 23:01:16 -0500

    fix login

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define('DB_NAME', 'urcscon3_Los-Angeles');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
