0c4c0d72c317018858f0421d1abc590be25b299b
Reeshad Rahman <rrahman5@u.rochester.edu>
2018-12-12 22:14:59 -0500

    admin priv

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', '66.147.242.186');
+define('DB_USERNAME', 'urcscon3_la');
+define('DB_PASSWORD', 'Coffee1N/!');
+define('DB_NAME', 'urcscon3_Los-Angeles');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
